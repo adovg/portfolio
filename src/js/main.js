f2110a7c18a0f42d5d6366873623f942fa3defbf
adovg <you@example.com>
2020-08-01 20:00:25 +0300

    preloader

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,6 @@
   
 
 $(document).ready(function () {
-	console.log('document ready');
 	
 	$('.works__btn button, .works__btn a').on('focus', function() {
 		 $(this).parents('.works__element').addClass('works__element--active');
@@ -29,6 +28,9 @@ $(document).ready(function () {
 		})
 	};
 
+
+
+	
 	
 	let animate = true;
 
@@ -42,32 +44,59 @@ $(window).scroll(function () {
  }
 });
 
-// jQuery(function($){ ...your code... });
-
-$('.carousel').owlCarousel({
-    loop:true,
-    margin:0,
-	nav:true,
-	dots:false,
-	navText: [],
-    responsive:{
-        0:{
-            items:1
-        },
-        480:{
-            items:2
-        },
-        768:{
-            items:3
-		},
-		980:{
-            items:5
-        }
-    }
-})
-
 
 });
+
+
+
+
+
+
+
+
+$('.works__btn button, .works__btn a').on('focus', function() {
+	$(this).parents('.works__element').addClass('works__element--active');
+});
+
+$('.works__btn button, .works__btn a').on('blur', function(e) {
+   $(this).parents('.works__element').removeClass('works__element--active');
+});
+
+function moveProgressBar(node, nodeLine, tooltip, animationLength = 600) {
+   const progressElement = $(node);
+   progressElement.each(function (value, item) {
+	   $(item).find(nodeLine).animate({
+		   width: item.dataset.progressPercent+'%'
+	   }, animationLength);
+	   $(item).find(tooltip).show(animationLength);
+   })
+};
+
+
+
+
+
+let animate = true;
+
+$(window).scroll(function () {
+
+if ($('.skills').offset().top <= $(window).scrollTop() + 100 ) {
+   if (animate) {
+   moveProgressBar('.progress__element', '.progress__line', '.progess__tooltip');
+	}
+	animate = false;
+}
+});
+
+
+
+
+
+
+
+
+
+
 
 
 var TxtRotate = function(el, toRotate, period) {
@@ -129,3 +158,29 @@ var TxtRotate = function(el, toRotate, period) {
 	document.body.appendChild(css);
   };
 
+
+
+
+//   if (window.onload) {
+// 	console.log('DOM готов');
+// 	let delay = () => {
+// 	   let preloaderEl = document.getElementById('preloader');
+// 	preloaderEl =  preloaderEl.classList.toggle('hidden');
+// 	};
+// 	setTimeout(delay, 1000);
+//   }
+
+  let ready = () => {
+	// console.log('DOM готов');
+	// let preloaderEl = document.getElementById('preloader');
+	//  preloaderEl =  preloaderEl.classList.toggle('hidden');
+
+	let delay = () => {
+		let preloaderEl = document.getElementById('preloader');
+	 preloaderEl =  preloaderEl.classList.toggle('hidden');
+	 };
+	 setTimeout(delay, 1000);
+  };
+
+
+  document.addEventListener("DOMContentLoaded", ready);
